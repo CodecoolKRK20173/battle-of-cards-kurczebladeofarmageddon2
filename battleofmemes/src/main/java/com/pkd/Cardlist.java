fcaf70e721c14092f95a8f1c8874246e26175822
David <cichy.dawid@gmail.com>
2019-06-04 11:51:13 +0200

    Add splitCardsOn() to Cardlist

--- battleofmemes/src/main/java/com/pkd/Cardlist.java
+++ battleofmemes/src/main/java/com/pkd/Cardlist.java
@@ -1,10 +1,12 @@
 package com.pkd;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.LinkedList;
 
 public class Cardlist {
     private LinkedList<Card> cardList;
+
     public Cardlist(){
         this.cardList = new LinkedList<>();
     }
@@ -13,9 +15,21 @@ public class Cardlist {
         cardList.add(card);
     }
 
-    // public ArrayList<LinkedList<Card>> splitCardsOn(int numbersOfDecs){
-    //     ArrayList<LinkedList<Card>> decks = new ArrayList<LinkedList<Card>>();
-    //     int numberOfCards = cardList.size();
-        // TODO
-    // }
+    public ArrayList<LinkedList<Card>> splitCardsOn(int numbersOfDecs){
+        ArrayList<LinkedList<Card>> decks = new ArrayList<LinkedList<Card>>();
+        int numberOfCards = cardList.size();
+        int lastDeckIndex = numbersOfDecs - 1;
+        LinkedList<Card> shuffeledCards = new LinkedList<Card>(this.cardList);
+        Collections.shuffle(shuffeledCards);
+        for (int j = 0; j < numbersOfDecs; j++)
+            decks.add(new LinkedList<Card>());
+
+        int j = 0;
+        for (int i = 0; i<numberOfCards; i ++){
+            decks.get(j).add(shuffeledCards.get(i));
+            j++;
+            if (j>lastDeckIndex) j = 0;
+        }
+        return decks;
+    }
 }

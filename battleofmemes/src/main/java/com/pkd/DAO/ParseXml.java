66fd07f047add234105ff730749afcedda93d72f
Kamil Kiklica <kamil.kiklica@gmail.com>
2019-06-04 12:58:42 +0200

    Add ParseXML and Cards.xml files

---
+++ battleofmemes/src/main/java/com/pkd/DAO/ParseXml.java
@@ -0,0 +1,88 @@
+import org.w3c.dom.*;
+import javax.xml.parsers.*;
+import java.io.*;
+import java.util.LinkedList;
+
+public class ParseXml {
+
+    public static void main(String[] args) {
+
+        LinkedList<Card> cardList = new LinkedList<>();
+
+        try {
+            File inputFile = new File("Cards.xml");
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = factory.newDocumentBuilder();
+            Document doc = dBuilder.parse(inputFile);
+            doc.getDocumentElement().normalize();
+            System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
+            NodeList factList = doc.getElementsByTagName("Card");
+            System.out.println("----------------------------");
+
+            for (int temp = 0; temp < factList.getLength(); temp++) {
+                        Node factNode = factList.item(temp);
+                        //System.out.println("\n<Fact>" + factNode.getNodeName()); // <Fact>
+                        System.out.println("\n"); 
+
+                if (factNode.getNodeType() == Node.ELEMENT_NODE) {
+                    Element eElement = (Element) factNode;
+                    System.out.println("<Card id=> " 
+                       + eElement.getAttribute("name"));
+                    NodeList descriptionList = eElement.getElementsByTagName("Description");
+
+                   for (int tempp = 0; tempp < descriptionList.getLength(); tempp++) {
+                    Node descriptionNode = descriptionList.item(tempp);
+    
+//                    Node descriptionNode = descriptionList.item(temp);
+
+
+                    if (descriptionNode.getNodeType() == Node.ELEMENT_NODE) {
+                        Element eeElement = (Element) descriptionNode;
+                        //System.out.println("Fact Element: " + descriptionNode.getNodeName());
+                        System.out.println("<Description value=>" //<Description value=>
+                            + eeElement.getAttribute("value")); 
+                        NodeList evalsList = eElement.getElementsByTagName("Attributes");
+//                        Node evalsNode = evalsList.item(temp);
+
+                        for (int cooun = 0; cooun < evalsList.getLength(); cooun++) {
+                        Node evalsNode = evalsList.item(cooun);
+
+
+                        if (evalsNode.getNodeType() == Node.ELEMENT_NODE) {
+                            Element eeeElement = (Element) evalsNode;
+                            System.out.println("<"+ evalsNode.getNodeName()+">"); //<Evals>
+                            NodeList evalList = eeeElement.getElementsByTagName("Attribute");
+                            
+                            for (int temps = 0; temps < evalList.getLength(); temps++) {
+                            Node node1 = evalList.item(temps);
+                                
+                                if (node1.getNodeType() == node1.ELEMENT_NODE) {
+                                    Element car = (Element) node1;
+                                    System.out.print("Attribute id =");
+                                    System.out.println(car.getAttribute("id")); // family/money/comfort
+                                    System.out.print("Value "+ car.getAttribute("id")+" ="); //Value family/money/... =
+                                    System.out.println(car.getTextContent()); //boolean result
+
+
+                                }
+                            }
+
+                        }
+                        
+                    }
+                }
+            }
+        }}
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+         }
+
+
+
+    
+
+
+
+    }
+}
